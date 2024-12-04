234e63ca5051e1e68b6614eaec2ea059eab0d73f
FilipczukJakub <filipczukjakub@gmail.com>
2024-12-04 20:32:14 +0100

    some_improvements

--- camera.py
+++ camera.py
@@ -6,31 +6,39 @@ import json
 import os
 from picamera2 import Picamera2, Preview
 import time
+from io import BytesIO
 global_stop = False
-
+image = None
 async def echo(websocket):
     global global_stop
+    global image
     while not global_stop:
-        await websocket.send()
-        asyncio.sleep(0.2)
+        local_image = image
+        buffer = BytesIO()
+        local_image.save(buffer, format='JPEG')
+        image_bytes = buffer.getvalue()
+        buffer.close()
+        await websocket.send(image_bytes)
+        await asyncio.sleep(0.2)
 
-async def camera_feed():
+def camera_feed():
     global global_stop
+    global image
     picam2 = Picamera2()
-    picam2.start_preview(Preview.QLGL)
+    picam2.start_preview(Preview.QTGL)
     preview_config = picam2.create_preview_configuration()
     capture_config = picam2.create_preview_configuration()
     picam2.configure(capture_config)
     picam2.start()
     while not global_stop:
         image = picam2.capture_image()
-        print(image)
+        time.sleep(0.2)
 
 async def server(ip,stop):
-    async with websockets.serve(echo,'0.0.0.0',8766):
+    async with websockets.serve(echo,'0.0.0.0',8767):
         print("Camera feed is beeing send")
         await stop
-    print("Camera feed stopped")
+    print("Camer a feed stopped")
 
 async def main():
     global global_stop
